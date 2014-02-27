f8d3c7761cae3ab49e09e75da776eecf92843c6c
Patrick Rasmussen <mcman407@gmail.com>
2014-02-27 15:48:18 -0500

    Update

---
+++ Assets/Scripts/GroundFireworks.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundFireworks : MonoBehaviour {
+	public Transform player;
+	public GameObject [] fireworksChilds;
+	// Use this for initialization
+	void Awake()
+	{
+		for (int i = 0; i < fireworksChilds.Length; i++) 
+		{
+			fireworksChilds[i].SetActive(false);
+		}
+	}
+	void FireFireworks(int i)
+	{
+		fireworksChilds[i].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		for (int i = 0; i < fireworksChilds.Length; i++) 
+		{
+			float distance = Vector3.Distance(fireworksChilds[i].transform.position, player.position);
+			if(distance < 18)
+			{
+				FireFireworks(i);
+			}
+			else if(distance > 30)
+			{
+
+			}
+		}
+	}
+}
